8be14b476557c42c9c2ce8119b23a1eef421e209
NIKHIL KATHURIA <nikhil-kathuria@users.noreply.github.com>
2015-03-08 05:58:40 -0400

    Added PlayMusic_API
    Added PlayMusic_API to play .wav files

---
+++ PlayMusic_API.py
@@ -0,0 +1,33 @@
+"""
+PlayMusic_API.py works onlt for .wav files 
+Import pyAudio and wave modules
+"""
+import pyaudio
+import wave
+
+
+class AudioFile:
+    chunk = 1024
+
+    def __init__(self, file):
+        """ Init audio stream """
+        self.wf = wave.open(file, 'rb')
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format=self.p.get_format_from_width(self.wf.getsampwidth()),
+            channels=self.wf.getnchannels(),
+            rate=self.wf.getframerate(),
+            output=True
+        )
+
+    def play(self):
+        """ Play entire file """
+        data = self.wf.readframes(self.chunk)
+        while data != '':
+            self.stream.write(data)
+            data = self.wf.readframes(self.chunk)
+
+    def close(self):
+        """ Graceful shutdown """
+        self.stream.close()
+        self.p.terminate()
